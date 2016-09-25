a2404732b4d4478b5bdbab59e9dcc6793d987980
Joosep Tenn <joosep41@email.com>
2016-09-25 16:02:19 +0300

    Base classes definition

---
+++ ExperienceSamplingApp/app/src/main/java/com/example/madiskar/experiencesamplingapp/Question.java
@@ -0,0 +1,16 @@
+package com.example.madiskar.experiencesamplingapp;
+
+/**
+ * Created by Joosep on 25.09.2016.
+ */
+public class Question {
+    private String text;
+
+    public Question(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
