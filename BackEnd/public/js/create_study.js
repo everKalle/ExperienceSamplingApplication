5ef39fe610e74622ce192723e0a7ba434f09359d
Kalle Ever <ever.kalle@gmail.com>
2016-09-27 00:17:33 +0300

    added missing js

---
+++ BackEnd/public/js/create_study.js
@@ -0,0 +1,228 @@
+var questionCount = 0;
+
+function add_question(){
+	$("#questionHolder").append('<div class="panel panel-default" id="question-panel-' + questionCount + '">' + 
+		  '<div class="panel-body">' +
+		  	'<div class="form-horizontal">' +
+		      '<div class="form-group">' +
+		        '<label class="control-label col-sm-3" for="question-type-' + questionCount + '">Küsimuse tüüp: </label>' +
+		        '<div class="col-sm-7">' + 
+				    '<select class="form-control input-sm" name="question-type-' + questionCount + '" id="question-type-' + questionCount + '" onChange="modifyType(' + questionCount + ');">' +
+				      '<option value="freetext" selected>Vabatekst</option>' + 
+				      '<option value="multichoice">Valikvastustega</option>' + 
+				    '</select>' + 
+			    '</div>' + 
+		      '</div>' + 
+		      	'<div class="form-group">' + 
+			        '<label class="control-label col-sm-3" for="question-title-' + questionCount + '">Küsimus: </label>' + 
+			        '<div class="col-sm-7">' + 
+			          '<input class="form-control" type="text" id="question-title-' + questionCount + '" name="question-title-' + questionCount + '" size="100" placeholder="Küsimus" required/>' + 
+			        '</div>' + 
+			      '</div>' + 
+			  '<div id="question-data-holder-' + questionCount + '">' + 
+		      '</div>' + 
+		    '</div>' + 
+		  '</div>' + 
+		'</div>');
+	modifyType(questionCount);
+	$('#question-panel-' + questionCount).hide();
+	$('#question-panel-' + questionCount).slideDown(200);
+	questionCount++;
+	$("#study-question-count").val(questionCount);
+	$("#question-remove-button").show();
+	$("#submit-button").prop('disabled', false);
+	$('#alert-question-add').slideUp(200);
+}
+
+function remove_question(){
+	var r = confirm("Kustuta viimane küsimus?");
+	if (r) {
+		if (questionCount > 0){
+			$("#question-panel-" + (questionCount-1)).remove();
+			questionCount--;
+			$("#study-question-count").val(questionCount);
+			if (questionCount == 0){
+				$("#question-remove-button").hide();
+				$("#submit-button").prop('disabled', true);
+				$('#alert-question-add').slideDown(200);
+			}
+		}
+	}
+}
+
+function modifyType(index){
+	var type = $("#question-type-" + index).val();
+	if (type == "freetext"){
+		$("#question-data-holder-" + index).html('');
+	} else if (type == "multichoice") {
+		$("#question-data-holder-" + index).html('<hr>'+
+						'<div class="form-group">'+
+					    '<label class="control-label col-sm-3" for="question-multichoice-single-choice-' + index + '">Valida saab: </label>'+
+					      '<div class="col-sm-7">'+
+					      	'<input type="radio" name="question-multichoice-single-choice-' + index + '" value="1" checked> Ühe vastusevariandi<br>' +
+          					'<input type="radio" name="question-multichoice-single-choice-' + index + '" value="0"> Mitu vastusevarianti' +
+					    '</div>'+
+					  '</div>' +
+						'<hr>'+
+			  		  '<div class="form-group">'+
+					    '<label class="control-label col-sm-3" for="question-multichoice-' + index + '-0">Vastusevariandid: </label>'+
+					      '<div class="col-sm-7">'+
+					      '<input class="form-control" type="text" id="question-multichoice-' + index + '-0" name="question-multichoice-' + index + '-0" size="100" placeholder="Vastusevariant 1" required/>'+
+					    '</div>'+
+					  '</div>'+
+					  '<div class="form-group">'+
+					    '<label class="control-label col-sm-3" for="question-multichoice-' + index + '-1">&nbsp;</label>'+
+					      '<div class="col-sm-7">'+
+					      '<input class="form-control" type="text" id="question-multichoice-' + index + '-1" name="question-multichoice-' + index + '-1" size="100" placeholder="Vastusevariant 2" required/>'+
+					    '</div>'+
+					  '</div>' +
+					  '<div class="form-group" id="multichoice-add-group-' + index + '">'+
+					  	'<label class="control-label col-sm-3" for="no-target">&nbsp;</label>'+
+					  	'<div class="col-sm-7">'+
+					  		'<button id="multichoice-add-button-' + index + '" type="button" class="btn btn-default" onClick="addMultichoice(' + index + ',2);">Lisa vastusevariant</button>'+
+					  	'</div>'+
+					  '</div>')
+	}
+}
+
+function addMultichoice(index, choice_index){
+	$("#multichoice-add-group-" + index).remove();
+	$("#question-data-holder-" + index).append('<div class="form-group" id="multichoice-answer-group-' + index + '-' + choice_index + '">'+
+					    '<label class="control-label col-sm-3" for="question-multichoice-' + index + '-' + choice_index + '">&nbsp;</label>'+
+					      '<div class="col-sm-7">'+
+					      '<input class="form-control" type="text" id="question-multichoice-' + index + '-' + choice_index + '" name="question-multichoice-' + index + '-' + choice_index + '" size="100"  placeholder="Vastusevariant ' + (choice_index + 1) + '" required/>'+
+					    '</div>'+
+					  '</div>');
+	$("#question-data-holder-" + index).append('<div class="form-group" id="multichoice-add-group-' + index + '">'+
+					  	'<label class="control-label col-sm-3" for="no-target">&nbsp;</label>'+
+					  	'<div class="col-sm-7">'+
+					  		'<button id="multichoice-add-button-' + index + '" type="button" class="btn btn-default" onClick="addMultichoice(' + index + ',' + (choice_index + 1) + ');">Lisa vastusevariant</button>&nbsp;&nbsp;'+
+					  		'<button id="multichoice-remove-button-' + index + '" type="button" class="btn btn-danger" onClick="removeMultichoice(' + index + ',' + choice_index + ');">Eemalda viimane vastusevariant</button>' +
+					  	'</div>'+
+					  '</div>');
+}
+
+function removeMultichoice(index, choice_index){
+	$("#multichoice-add-group-" + index).remove();
+	$("#multichoice-answer-group-" + index + "-" + choice_index).remove();
+	var removeButtonString = '<button id="multichoice-remove-button-' + index + '" type="button" class="btn btn-danger" onClick="removeMultichoice(' + index + ',' + (choice_index - 1) + ');">Eemalda viimane vastusevariant</button>';
+	if (choice_index == 2) {
+		removeButtonString = '';
+	}
+	$("#question-data-holder-" + index).append('<div class="form-group" id="multichoice-add-group-' + index + '">'+
+					  	'<label class="control-label col-sm-3" for="no-target">&nbsp;</label>'+
+					  	'<div class="col-sm-7">'+
+					  		'<button id="multichoice-add-button-' + index + '" type="button" class="btn btn-default" onClick="addMultichoice(' + index + ',' + choice_index + ');">Lisa vastusevariant</button>&nbsp;&nbsp;'+
+					  		removeButtonString +
+					  	'</div>'+
+					  '</div>');
+}
+
+var eventCount = 0;
+function add_event(){
+	$("#eventHolder").append('<div class="panel panel-default" id="event-panel-' + eventCount + '">' +
+			'<div class="panel-body">' +
+		  	'<div class="form-horizontal">' +
+		      	'<div class="form-group">' +
+			        '<label class="control-label col-sm-3" for="event-title-' + eventCount + '">Sündmuse nimi: </label>' +
+			        '<div class="col-sm-7">' +
+			          '<input class="form-control" type="text" id="event-title-' + eventCount + '" name="event-title-' + eventCount + '" size="100" placeholder="Sündmuse nimi" required/>' +
+			        '</div>' +
+			    '</div>' +
+			    '<div class="form-group">' +
+			        '<label class="control-label col-sm-3" for="event-control-time-' + eventCount + '">Kontrollaeg: </label>' +
+			        '<div class="col-sm-7 form-inline">' +
+			          	'<input class="form-control input-sm" type="text" id="event-control-time-' + eventCount + '" name="event-control-time-' + eventCount + '" size="5" placeholder="0" required/>&nbsp;&nbsp;' +
+          				'<select class="form-control input-sm" name="event-control-time-unit-' + eventCount + '">' +
+			            '<option value="eng">minutit</option>' +
+			            '<option value="rus" selected>tundi</option>' +
+			            '<option value="ger">päeva</option>' +
+			          '</select>' +
+			        '</div>' +
+			    '</div>' +
+		    '</div>' +
+		  '</div>' +
+		'</div>');
+	$('#event-panel-' + eventCount).hide();
+	$('#event-panel-' + eventCount).slideDown(200);
+	eventCount++;
+	$("#study-event-count").val(eventCount);
+	$("#event-remove-button").show();
+}
+
+function remove_event(){
+	var r = confirm("Kustuta viimane sündmus?");
+	if (r) {
+		if (eventCount > 0){
+			$("#event-panel-" + (eventCount-1)).remove();
+			eventCount--;
+			$("#study-event-count").val(eventCount);
+			if (eventCount == 0){
+				$("#event-remove-button").hide();
+			}
+		}
+	}
+}
+
+$("#study-start-date").on("propertychange keyup paste input", function(){
+	//$('#study-start-date').setCustomValidity("errorMessage");
+	if (($("#study-start-date").val().length) == 10){
+	    var date = new Date($("#study-start-date").val());
+	    if (date instanceof Date){
+	    	if (date.toString() != "Invalid Date"){
+		    	$("#study-start-date-group").removeClass("has-error");
+		    	comparedates();
+		    }
+	    } else {
+	    	$("#study-start-date-group").addClass("has-error");
+	    }
+    } else {
+    	$("#study-start-date-group").addClass("has-error");
+    }
+});
+
+$("#study-end-date").on("propertychange keyup paste input", function(){
+	//$('#study-start-date').setCustomValidity("errorMessage");
+	if (($("#study-end-date").val().length) == 10){
+	    var date = new Date($("#study-end-date").val());
+	    if (date instanceof Date){
+	    	if (date.toString() != "Invalid Date"){
+		    	$("#study-end-date-group").removeClass("has-error");
+		    	comparedates();
+	    	}
+	    } else {
+	    	$("#study-end-date-group").addClass("has-error");
+	    }
+    } else {
+    	$("#study-end-date-group").addClass("has-error");
+    }
+});
+
+function comparedates(){
+	if (($("#study-end-date").val().length) == 10 && ($("#study-start-date").val().length) == 10){
+	    var endDate = new Date($("#study-end-date").val());
+	    var startDate = new Date($("#study-start-date").val());
+	    if (endDate instanceof Date && startDate instanceof Date && startDate.toString() != "Invalid Date" && endDate.toString() != "Invalid Date"){
+		    if (startDate < endDate){
+		    	$("#study-start-date-group").removeClass("has-error");
+		    	$("#study-end-date-group").removeClass("has-error");
+		    	$("#study-date-help").hide();	
+		    } else {
+		    	$("#study-start-date-group").addClass("has-error");
+		    	$("#study-end-date-group").addClass("has-error");
+		    	$("#study-date-help").show();
+		    }
+	    	console.log(startDate);
+	    }
+	}
+}
+
+/*function validate_start_date(field){
+	field.setCustomValidity('Vigane kuupäev');
+	if (($("#study-start-date").val().length) == 10){
+	    var date = new Date($("#study-start-date").val());
+	    if (date instanceof Date){
+	    	
+	    }
+    }
+}*/
