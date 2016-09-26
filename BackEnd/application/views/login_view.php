be4d0e8671a25158ed57733d273af846eabc3a80
Kaspar Kliimask <kaspar94@tdl.ee>
2016-09-26 14:34:10 +0300

    Login view. closes #10

--- BackEnd/application/views/login_view.php
+++ BackEnd/application/views/login_view.php
@@ -1,10 +1,11 @@
-   <?php echo validation_errors(); ?>
-   <?php echo form_open('VerifyLogin'); ?>
-     <label for="username">Username:</label>
-     <input type="text" size="20" id="username" name="username"/>
-     <br/>
-     <label for="password">Password:</label>
-     <input type="password" size="20" id="password" name="password"/>
-     <br/>
-     <input type="submit" value="Login"/>
-   </form>
+<div class="container" style="max-width:330px;">
+  	 <?php echo validation_errors(); ?>
+	   <?php echo form_open('VerifyLogin', array('class' => 'form-signin')); ?>
+	<p class="text-center"> E-posti aadress </p>
+        <input type="text" name="username" id="inputText" class="form-control" placeholder="Kasutajanimi" required autofocus>
+	<p class="text-center" style="margin-top:10px;"> Parool </p>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" required>
+        <button class="btn btn-sm btn-primary center-block" type="submit" style="margin-top:10px; ">Logi sisse</button>
+      </form>
+
+    </div> <!-- /container -->
